efb5ee04b2c1721197d4985a8b8c3d8203b51576
Dave Esterman <daveesterman@gmail.com>
2013-09-19 00:53:10 -0400

    Create first unit tests and .gitignore
    Create some unit tests for the EuclidianDistance algorithm.  Created a
    .gitignore file in order to track the excludes with the project.
    Put most of the __init__.py code under 'main' because the unit tests
    didn't like it outside of 'main'.

--- src/collaborativefiltering/__init__.py
+++ src/collaborativefiltering/__init__.py
@@ -8,15 +8,15 @@ import numpy as np
 from collaborativefiltering.filter import Filter
 from collaborativefiltering.similarityscorealgorithms import EuclidianDistance
 
-data = {}
-
-#read data from file
-with open('../../data/PCI.ch2.critics.txt', encoding='utf-8') as a_file:
-    data = eval(a_file.read())
-
-f = Filter(data, EuclidianDistance())
-
-
 if __name__ == '__main__':
+
+    data = {}
+
+    # read data from file
+    with open('../../data/PCI.ch2.critics.txt', encoding='utf-8') as a_file:
+        data = eval(a_file.read())
+
+    f = Filter(data, EuclidianDistance())
+
     print(f.computeScoreTwo('Lisa Rose', 'Jack Matthews'))
     print(f.computeScoreTwo('Lisa Rose', 'Gene Seymour'))
