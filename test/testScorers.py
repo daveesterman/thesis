2e92f293d7666f50d0daa68300a9873da6fc3650
Dave Esterman <daveesterman@gmail.com>
2013-09-19 20:23:15 -0400

    Fixed unit test.  Equal vectors weren't equal.

--- test/testScorers.py
+++ test/testScorers.py
@@ -17,7 +17,7 @@ class TestEuclidianDistance(unittest.TestCase):
     def testPerfectMatch(self):
         'equal vectors should have a score of 1.0'
         vec1 = np.array([1, 2, 3])
-        vec2 = np.array([1, 2, 4])
+        vec2 = np.array([1, 2, 3])
         score = self.euclid.score(vec1, vec2)
         self.assertEqual(score, 1.0, 'score not equal to 1.0')
         
